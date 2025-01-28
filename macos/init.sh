cc0181dfdac92a3efbef5ae72d909dab196e0628
Hyogeun Oh <ohg3417@gmail.com>
2025-01-28 14:29:05 +0900

    :hammer: add: skhd service start

--- macos/init.sh
+++ macos/init.sh
@@ -19,6 +19,7 @@ brew install koekeishiya/formulae/skhd
 # sudo nvram boot-args=-arm64e_preview_abi
 # echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai
 # yabai --start-service
+# skhd --start-service
 
 # -------- Dev -------- #
 brew install --cask miniconda
