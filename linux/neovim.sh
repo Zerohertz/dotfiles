9ef995ab8cceb42a7474dc71221e96793f116046
Zerohertz <ohg3417@gmail.com>
2025-02-17 17:36:08 +0900

    :hammer: update: install neovim on linux

--- linux/neovim.sh
+++ linux/neovim.sh
@@ -1,8 +1,8 @@
 # ----------------------- ROOT ----------------------- #
-wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz &&
-	tar -xzf nvim-linux64.tar.gz &&
-	cp -r nvim-linux64/* /usr/local/ &&
-	rm nvim-linux64.tar.gz && rm -rf nvim-linux64 &&
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz &&
+	tar -xzf nvim-linux-x86_64.tar.gz &&
+	cp -r nvim-linux-x86_64/* /usr/local/ &&
+	rm nvim-linux-x86_64.tar.gz && rm -rf nvim-linux-x86_64 &&
 	curl -fsSL https://deb.nodesource.com/setup_20.x | bash - &&
 	apt-get install -y ripgrep fd-find fzf fish luarocks nodejs &&
 	npm install -g neovim tree-sitter-cli
