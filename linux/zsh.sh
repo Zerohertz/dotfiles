9c2406e84fe065a8073ba896a9b26bcc584e8052
Zerohertz <ohg3417@gmail.com>
2026-05-05 21:22:49 +0900

    :hammer: update: sudo for install zsh
    Signed-off-by: Zerohertz <ohg3417@gmail.com>

--- linux/zsh.sh
+++ linux/zsh.sh
@@ -1,15 +1,17 @@
+#!/usr/bin/env bash
+
 rm -f ~/.zshrc
 rm -f ~/.p10k.zsh
 rm -rf ~/.oh-my-zsh
 
-if [ "$EUID" -eq 0 ]; then
-	apt-get update &&
-		apt-get install zsh -y &&
-		apt-get clean && rm -rf /var/lib/apt/lists/*
-else
-	sudo apt-get update &&
-		sudo apt-get install zsh -y &&
-		sudo apt-get clean && rm -rf /var/lib/apt/lists/*
+SUDO=""
+[ "${EUID}" -ne 0 ] && SUDO="sudo"
+
+if ! command -v zsh >/dev/null 2>&1; then
+    ${SUDO} apt-get update &&
+        ${SUDO} apt-get install zsh -y &&
+        ${SUDO} apt-get clean &&
+        ${SUDO} rm -rf /var/lib/apt/lists/*
 fi
 
 echo "n" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -17,8 +19,5 @@ echo "n" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-curl -fsSL https://raw.githubusercontent.com/Zerohertz/dotfiles/main/.zshrc >$HOME/.zshrc
-curl -fsSL https://raw.githubusercontent.com/Zerohertz/dotfiles/main/.p10k.zsh >$HOME/.p10k.zsh
 
 echo "\n\033[1;36m⭐ ZSH (powerlevel10k) Installed! ⭐\n\n\033[0m\tRun the following command to switch to ZSH:\n\033[1;32m\tchsh -s \$(which zsh)\033[0m"
